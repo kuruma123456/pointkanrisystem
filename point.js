2b6ac56ea057d29d77951acd04cef46ad93a7d26
kuruma123456 <125453332+kuruma123456@users.noreply.github.com>
2023-04-14 07:31:44 +0900

    Update point.js

--- point.js
+++ point.js
@@ -1,1 +1,11 @@
+// Scripting.FileSystemObject というオブジェクトを作成（JavaScript内でWSHを使ってファイルを扱う）
+      var fs = new ActiveXObject("Scripting.FileSystemObject");
 
+      // text.txtという新規のファイルを作成
+      var file = fs.CreateTextFile("point.txt");
+
+      // text.txtファイルへ書き込み
+      file.Write(point);
+
+      // text.txtファイルを閉じる
+      file.Close();
